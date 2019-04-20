9f5ced373e2d1402e4f9054e02408295701f0ad4
Gui-dev <adrianon10@gmail.com>
2019-04-20 02:48:22 -0300

    Estrutura do backend

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require( 'express' )
+const mongoose = require( 'mongoose' )
+const routes = require( './routes' )
+const path = require( 'path' )
+const socket = require( 'socket.io' )
+const http = require( 'http' )
+const cors = require( 'cors' )
+
+const app = express()
+app.use( cors() )
+const server = http.Server( app )
+const io = socket(server)
+const port = process.env.PORT || 3333
+
+
+io.on( 'connection', socket => {
+
+  socket.on( 'connectRoom', box => {
+    socket.join( box )
+  } )
+} )
+
+mongoose.connect( 'mongodb+srv://gui:gui@cluster0-cvlc0.mongodb.net/omnistack?retryWrites=true', 
+    { useNewUrlParser: true } 
+)
+
+app.use( ( req, res, next ) => {
+  req.io = io
+  return next()
+} )
+app.use( express.json() )
+app.use( express.urlencoded( { extended: true } ) )
+app.use( '/files', express.static( 
+  path.resolve( __dirname, '..', 'tmp' )
+ ) )
+app.use( routes )
+
+
+server.listen( port, () => {
+
+  console.log( `Servidor rodando na porta ${port}` )
+} )
